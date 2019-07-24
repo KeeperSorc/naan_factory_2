16b313a0870e8f93879542bf7d46eb4c4c87fdd4
Oscar China <OChina@spartaglobal.com>
2019-07-24 16:26:01 +0100

    debugged naan_factory_functions file. created three tests in naan_factory_tests file

--- naan_factory_functions.py
+++ naan_factory_functions.py
@@ -1,4 +1,16 @@
 #Functions to be used in the naan factory
 
-#Make dough function - takes in wheat and water, returns dough
+#make_dough function - takes in the amounts of wheat and water, then returns the approximate amount of dough
 def make_dough(wheat_arg,water_arg):
+    dough_amount = wheat_arg + water_arg
+    return dough_amount
+
+#bake_naan function - takes in the amount of dough, then returns the approximate amount of baked naan
+def bake_naan(dough_arg):
+    naan_amount = float(dough_arg/2)
+    return naan_amount
+
+#deliver_naan function - takes in distance in miles, returns approx delivery time in minutes, assuming delivery speed is 10 miles per hour
+def deliver_naan(distance_arg):
+    delivery_time = (distance_arg/10)*60
+    return delivery_time
--- naan_factory_tests.py
+++ naan_factory_tests.py
@@ -1,1 +1,20 @@
 ##Testing
+import naan_factory_functions as nff
+
+#Test 1 - test make_dough function
+#spec - when called, should return int dough_amount
+print("test make_dough function, when called, should take in the amount of wheat and water, and return the dough amount")
+setup_result = nff.make_dough(10,20)
+print(setup_result)
+
+#Test 2 - test bake_naan function
+#spec -
+print("test bake_naan function, when called, should take in the amount of dough, and return the naan amount")
+setup_result = nff.bake_naan(setup_result)
+print(setup_result)
+
+#Test 3 - deliver_naan function
+#spec -
+print("test deliver_naan function, when called, should take in the distance for the delivery, and return delivery time")
+setup_result = nff.deliver_naan(30)
+print(setup_result)
