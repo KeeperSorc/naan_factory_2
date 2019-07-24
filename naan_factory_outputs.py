93dbb9c3cab303a8393866046958972aab0ed127
Oscar China <OChina@spartaglobal.com>
2019-07-24 16:29:32 +0100

    debugged naan_factory_outputs file

--- naan_factory_outputs.py
+++ naan_factory_outputs.py
@@ -3,5 +3,5 @@ import naan_factory_functions as nff
 naan_amount = nff.bake_naan(nff.make_dough(30,30))
 naan_delivery_time = nff.deliver_naan(20)
 
-print("You ordered " + naan_amount + " amount of naan.")
-print("Your naan will take " + naan_delivery_time + " minutes to deliver.")
+print("You ordered " + str(naan_amount) + " amount of naan.")
+print("Your naan will take " + str(naan_delivery_time) + " minutes to deliver.")
