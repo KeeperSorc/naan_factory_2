ce8e1e44e13602508b0b376a9eb4971159178246
Oscar China <OChina@spartaglobal.com>
2019-07-24 16:28:55 +0100

    created naan_factory_outputs file

--- naan_factory_outputs.py
+++ naan_factory_outputs.py
@@ -1,1 +1,7 @@
 #Print naan factory
+import naan_factory_functions as nff
+naan_amount = nff.bake_naan(nff.make_dough(30,30))
+naan_delivery_time = nff.deliver_naan(20)
+
+print("You ordered " + naan_amount + " amount of naan.")
+print("Your naan will take " + naan_delivery_time + " minutes to deliver.")
